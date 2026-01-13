2ffed633f8e5fda03cbfe9e0f9f9803dad9fc66a
Taleeb Chouhan <muhammadtaleeb086@gmail.com>
2026-01-13 19:57:24 +0530

    Add files via upload

---
+++ Prime_Number_checker.py
@@ -0,0 +1,16 @@
+number = int(input("enter a number: "))
+
+if number <= 1:
+    print("not a prime number")
+else:
+    is_prime = True
+
+    for i in range(2, number):
+        if number % i == 0:
+            is_prime = False
+            break
+
+    if is_prime:
+        print(f"{number} is a prime number")
+    else:
+        print(f"{number} is not a prime number")
