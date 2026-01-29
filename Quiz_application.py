8786cf1171526ce678bb7de10a1e9894c2a54bc3
Taleeb Chouhan <muhammadtaleeb086@gmail.com>
2026-01-29 23:42:10 +0530

    Add files via upload

--- Quiz_application.py
+++ Quiz_application.py
@@ -1,33 +1,52 @@
-# Quiz questions stored in tuples
 questions = (
-    ("What is the capital of India?", "a", ["a. New Delhi", "b. Mumbai", "c. Kolkata", "d. Chennai"]),
-    ("Which language is used for Data Science?", "b", ["a. Java", "b. Python", "c. C++", "d. HTML"]),
-    ("2 + 2 * 2 = ?", "c", ["a. 6", "b. 8", "c. 6", "d. 4"]),
+    ("What is the capital of India?",
+     "1. Mumbai", "2. Delhi", "3. Kolkata", "4. Chennai", 2),
+
+    ("Which language is used for Data Science?",
+     "1. Java", "2. C", "3. Python", "4. HTML", 3),
+
+    ("Which data type is immutable?",
+     "1. List", "2. Tuple", "3. Set", "4. Dictionary", 2),
+
+    ("Which keyword is used for loop in Python?",
+     "1. if", "2. while", "3. def", "4. return", 2)
 )
 
-score = 0  # initialize score
+total_score = 0
+total_questions = len(questions)
 
-# Loop through each question
+print("---Welcome to Quiz Game---")
+
 for q in questions:
-    print("\n" + q[0])  # print question
-    for option in q[2]:  # print options
-        print(option)
-    
-    answer = input("Enter your answer (a/b/c/d): ").lower()  # take input and convert to lowercase
-    
-    # Check if answer is correct
-    if answer == q[1]:
-        print("Correct!")
-        score += 1  # increase score
-    else:
-        print("Wrong! Correct answer is:", q[1])
+  print("\n" + q[0])
+  print(q[1])
+  print(q[2])
+  print(q[3])
+  print(q[4])
+  
+  user_answer = int(input("Enter your answer(1-4):"))
 
-# Show final result
-print("\nQuiz Completed!")
-print("Your Score:", score, "out of", len(questions))
-if score == len(questions):
-    print("Excellent! You got all correct.")
-elif score >= len(questions)//2:
-    print("Good! But you can do better.")
+  if user_answer == q[5]:
+    print("Correct Answer")
+    total_score += 1
+  else :
+    print("your answer is wrong..")
+    print("correct option is ", q[5])
+  
+percentage = (total_score / total_questions) * 100
+
+print("\n---- Quiz Result ----")
+print("Total Questions:", total_questions)
+print("Correct Answers:", total_score)
+print("Percentage:", percentage, "%")
+
+if percentage >= 80:
+    print("Result: Excellent ")
+elif percentage >= 50:
+    print("Result: Good ")
 else:
-    print("You need to practice more.")
+    print("Result: Needs Improvement ")
+
+print("Thank you for playing!")
+  
+
