6b2afaafef0fd06bf68059328a1d2ad2acfb640e
Taleeb Chouhan <muhammadtaleeb086@gmail.com>
2026-02-24 15:16:39 +0530

    Add files via upload

---
+++ Python Logging/logs/test.py
@@ -0,0 +1,10 @@
+from logger import logging
+
+def add(a,b):
+
+  logging.debug("The addition operation is taking place")
+  return a+b
+
+
+logging.debug("The addition fucntion is called")
+add(10,15)
