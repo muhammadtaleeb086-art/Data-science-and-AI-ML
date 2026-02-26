dc6bb35aee61d328ac6bbe45a980089bf62a46ef
Taleeb Chouhan <muhammadtaleeb086@gmail.com>
2026-02-26 16:16:07 +0530

    Add files via upload

---
+++ Multithreading and Multiprocessing/tempCodeRunnerFile.py
@@ -0,0 +1,16 @@
+### Multiprocessing with ProcessPoolExecutor
+
+from concurrent.futures import ProcessPoolExecutor
+import time
+
+def square_number(number):
+  time.sleep(1)
+  return f"Square of {number}: {number ** 2}"
+
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+with ProcessPoolExecutor(max_workers=3) as executor:
+  results = list(executor.map(square_number, number))
+
+for result in results:
+  print(result)
